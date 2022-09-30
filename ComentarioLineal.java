e16d4a37962d8f5eda20d220a0d99ccb735c84c3
alvaro <bernabeyalvaro@gmail.com>
2022-09-30 09:18:47 +0200

    first Commit

---
+++ ComentarioLineal.java
@@ -0,0 +1,7 @@
+public class ComentarioLineal {
+    public static void main(String[] args){
+		//System.out.println("Hola Mundo");
+        System.out.println("Adios");
+        System.out.println("Fin del Programa");
+}
+}
---
+++ ComentarioVariasLineas.java
@@ -0,0 +1,12 @@
+public class ComentarioVariasLineas {
+    public static void main(String[] args){
+		/*System.out.println("Hola Mundo");
+        System.out.println("Adios");
+        System.out.println("Cucu");
+        */
+        for(int i = 0; i < 6; /*ahora viene el paso*/ i++){
+            System.out.println(i + "");
+        }
+        System.out.println("Fin del Programa");
+}
+}
